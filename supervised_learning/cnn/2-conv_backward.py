bee2f559e8d4c9ba3738acd25fa25bf5bf24bc15
abfabs <albaeftimi@gmail.com>
2026-03-01 10:38:13 +0100

    padding

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -19,15 +19,14 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
     Returns:
         tuple: (dA_prev, dW, db)
-            dA_prev has shape (m, h_prev, w_prev, c_prev)
-            dW has shape (kh, kw, c_prev, c_new)
-            db has shape (1, 1, 1, c_new)
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev_w, c_new = W.shape
-    _, h_new, w_new, c_new_dz = dZ.shape
+    m_dz, h_new, w_new, c_new_dz = dZ.shape
     sh, sw = stride
 
+    if m_dz != m:
+        raise ValueError("dZ and A_prev must have the same batch size")
     if c_prev_w != c_prev:
         raise ValueError("W channels do not match A_prev channels")
     if c_new_dz != c_new:
@@ -36,22 +35,31 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         raise ValueError("padding must be 'same' or 'valid'")
 
     if padding == "valid":
-        ph = 0
-        pw = 0
+        pad_top = pad_bottom = 0
+        pad_left = pad_right = 0
     else:
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+        pad_h = max((h_new - 1) * sh + kh - h_prev, 0)
+        pad_w = max((w_new - 1) * sw + kw - w_prev, 0)
+
+        pad_top = pad_h // 2
+        pad_bottom = pad_h - pad_top
+        pad_left = pad_w // 2
+        pad_right = pad_w - pad_left
 
     A_pad = np.pad(
         A_prev,
-        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        pad_width=(
+            (0, 0),
+            (pad_top, pad_bottom),
+            (pad_left, pad_right),
+            (0, 0),
+        ),
         mode="constant",
         constant_values=0,
     )
 
     dA_pad = np.zeros_like(A_pad)
     dW = np.zeros_like(W)
-
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
     for i in range(h_new):
@@ -65,19 +73,19 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
             for k in range(c_new):
                 dz_ijk = dZ[:, i, j, k]  # (m,)
+                dz_reshaped = dz_ijk[:, None, None, None]
 
-                dW[:, :, :, k] += np.sum(
-                    a_slice * dz_ijk[:, None, None, None],
-                    axis=0,
-                )
+                dW[:, :, :, k] += np.sum(a_slice * dz_reshaped, axis=0)
+                dA_pad[:, i0:i1, j0:j1, :] += W[:, :, :, k] * dz_reshaped
 
-                dA_pad[:, i0:i1, j0:j1, :] += (
-                    W[:, :, :, k] * dz_ijk[:, None, None, None]
-                )
-
-    if ph == 0 and pw == 0:
+    if padding == "valid":
         dA_prev = dA_pad
     else:
-        dA_prev = dA_pad[:, ph:-ph, pw:-pw, :]
+        dA_prev = dA_pad[
+            :,
+            pad_top:pad_top + h_prev,
+            pad_left:pad_left + w_prev,
+            :,
+        ]
 
     return dA_prev, dW, db
