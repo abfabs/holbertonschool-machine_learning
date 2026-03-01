5052adb665f2d1ea1305c8a5dc41c91d307da91c
abfabs <albaeftimi@gmail.com>
2026-03-01 10:36:36 +0100

    backward

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""2-conv_backward.py
+Performs back propagation over a convolutional layer.
+"""
+
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """Performs back propagation over a convolutional layer.
+
+    Args:
+        dZ (np.ndarray): shape (m, h_new, w_new, c_new)
+        A_prev (np.ndarray): shape (m, h_prev, w_prev, c_prev)
+        W (np.ndarray): shape (kh, kw, c_prev, c_new)
+        b (np.ndarray): shape (1, 1, 1, c_new)
+        padding (str): 'same' or 'valid'
+        stride (tuple): (sh, sw)
+
+    Returns:
+        tuple: (dA_prev, dW, db)
+            dA_prev has shape (m, h_prev, w_prev, c_prev)
+            dW has shape (kh, kw, c_prev, c_new)
+            db has shape (1, 1, 1, c_new)
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev_w, c_new = W.shape
+    _, h_new, w_new, c_new_dz = dZ.shape
+    sh, sw = stride
+
+    if c_prev_w != c_prev:
+        raise ValueError("W channels do not match A_prev channels")
+    if c_new_dz != c_new:
+        raise ValueError("dZ channels do not match W output channels")
+    if padding not in ("same", "valid"):
+        raise ValueError("padding must be 'same' or 'valid'")
+
+    if padding == "valid":
+        ph = 0
+        pw = 0
+    else:
+        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
+        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+
+    A_pad = np.pad(
+        A_prev,
+        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode="constant",
+        constant_values=0,
+    )
+
+    dA_pad = np.zeros_like(A_pad)
+    dW = np.zeros_like(W)
+
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    for i in range(h_new):
+        i0 = i * sh
+        i1 = i0 + kh
+        for j in range(w_new):
+            j0 = j * sw
+            j1 = j0 + kw
+
+            a_slice = A_pad[:, i0:i1, j0:j1, :]  # (m, kh, kw, c_prev)
+
+            for k in range(c_new):
+                dz_ijk = dZ[:, i, j, k]  # (m,)
+
+                dW[:, :, :, k] += np.sum(
+                    a_slice * dz_ijk[:, None, None, None],
+                    axis=0,
+                )
+
+                dA_pad[:, i0:i1, j0:j1, :] += (
+                    W[:, :, :, k] * dz_ijk[:, None, None, None]
+                )
+
+    if ph == 0 and pw == 0:
+        dA_prev = dA_pad
+    else:
+        dA_prev = dA_pad[:, ph:-ph, pw:-pw, :]
+
+    return dA_prev, dW, db
