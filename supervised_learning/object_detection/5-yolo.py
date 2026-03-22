309890547eb5f5a7c83d10b2f64ab5f680029536
abfabs <albaeftimi@gmail.com>
2026-03-22 18:03:31 +0100

    p

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -12,7 +12,7 @@ class Yolo:
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Class constructor"""
-        self.model = K.models.load_model(model_path, compile=False)
+        self.model = K.models.load_model(model_path)
 
         with open(classes_path, "r", encoding="utf-8") as f:
             self.class_names = [line.strip() for line in f]
@@ -27,58 +27,54 @@ class Yolo:
         box_confidences = []
         box_class_probs = []
 
-        input_w = self.model.input.shape[1]
-        input_h = self.model.input.shape[2]
-
-        image_h = image_size[0]
-        image_w = image_size[1]
+        image_h, image_w = image_size
 
         for i, output in enumerate(outputs):
-            grid_h = output.shape[0]
-            grid_w = output.shape[1]
-            anchor_boxes = output.shape[2]
+            grid_h, grid_w, anchor_boxes, _ = output.shape
 
             t_x = output[..., 0]
             t_y = output[..., 1]
             t_w = output[..., 2]
             t_h = output[..., 3]
 
-            c_x = np.arange(grid_w).reshape(1, grid_w, 1)
-            c_x = np.tile(c_x, (grid_h, 1, anchor_boxes))
+            c_x, c_y = np.meshgrid(np.arange(grid_w), np.arange(grid_h))
+            c_x = np.expand_dims(c_x, axis=-1)
+            c_y = np.expand_dims(c_y, axis=-1)
 
-            c_y = np.arange(grid_h).reshape(grid_h, 1, 1)
-            c_y = np.tile(c_y, (1, grid_w, anchor_boxes))
+            anchor_w = self.anchors[i, :, 0]
+            anchor_h = self.anchors[i, :, 1]
 
             b_x = (1 / (1 + np.exp(-t_x)) + c_x) / grid_w
             b_y = (1 / (1 + np.exp(-t_y)) + c_y) / grid_h
 
-            anchor_w = self.anchors[i, :, 0].reshape((1, 1, anchor_boxes))
-            anchor_h = self.anchors[i, :, 1].reshape((1, 1, anchor_boxes))
-
-            b_w = (np.exp(t_w) * anchor_w) / input_w
-            b_h = (np.exp(t_h) * anchor_h) / input_h
+            b_w = (anchor_w * np.exp(t_w)) / self.model.input.shape[1]
+            b_h = (anchor_h * np.exp(t_h)) / self.model.input.shape[2]
 
             x1 = (b_x - (b_w / 2)) * image_w
             y1 = (b_y - (b_h / 2)) * image_h
             x2 = (b_x + (b_w / 2)) * image_w
             y2 = (b_y + (b_h / 2)) * image_h
 
-            box = np.stack((x1, y1, x2, y2), axis=-1)
+            box = np.zeros((grid_h, grid_w, anchor_boxes, 4))
+            box[..., 0] = x1
+            box[..., 1] = y1
+            box[..., 2] = x2
+            box[..., 3] = y2
             boxes.append(box)
 
             box_confidence = 1 / (1 + np.exp(-output[..., 4:5]))
-            box_confidences.append(box_confidence)
-
             box_class_prob = 1 / (1 + np.exp(-output[..., 5:]))
+
+            box_confidences.append(box_confidence)
             box_class_probs.append(box_class_prob)
 
         return boxes, box_confidences, box_class_probs
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
         """Filter boxes using objectness score and class probability"""
-        filtered_boxes = []
-        box_classes = []
-        box_scores = []
+        filtered_boxes = np.empty((0, 4))
+        box_classes = np.empty((0,), dtype=int)
+        box_scores = np.empty((0,))
 
         for i in range(len(boxes)):
             scores = box_confidences[i] * box_class_probs[i]
@@ -87,15 +83,38 @@ class Yolo:
 
             mask = class_scores >= self.class_t
 
-            filtered_boxes.append(boxes[i][mask])
-            box_classes.append(classes[mask])
-            box_scores.append(class_scores[mask])
-
-        filtered_boxes = np.concatenate(filtered_boxes, axis=0)
-        box_classes = np.concatenate(box_classes, axis=0)
-        box_scores = np.concatenate(box_scores, axis=0)
+            filtered_boxes = np.concatenate(
+                (filtered_boxes, boxes[i][mask]),
+                axis=0
+            )
+            box_classes = np.concatenate(
+                (box_classes, classes[mask]),
+                axis=0
+            )
+            box_scores = np.concatenate(
+                (box_scores, class_scores[mask]),
+                axis=0
+            )
 
         return filtered_boxes, box_classes, box_scores
+
+    def iou(self, box1, box2):
+        """Calculate intersection over union for two boxes"""
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2
+
+        x1 = np.maximum(b1_x1, b2_x1)
+        y1 = np.maximum(b1_y1, b2_y1)
+        x2 = np.minimum(b1_x2, b2_x2)
+        y2 = np.minimum(b1_y2, b2_y2)
+
+        intersection = np.maximum(0, x2 - x1) * np.maximum(0, y2 - y1)
+
+        area1 = (b1_x2 - b1_x1) * (b1_y2 - b1_y1)
+        area2 = (b2_x2 - b2_x1) * (b2_y2 - b2_y1)
+        union = area1 + area2 - intersection
+
+        return intersection / union
 
     def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
         """Apply non-max suppression to filtered boxes"""
@@ -106,51 +125,26 @@ class Yolo:
         unique_classes = np.unique(box_classes)
 
         for cls in unique_classes:
-            cls_indices = np.where(box_classes == cls)[0]
+            class_indices = np.where(box_classes == cls)[0]
+            class_boxes = filtered_boxes[class_indices]
+            class_scores = box_scores[class_indices]
 
-            cls_boxes = filtered_boxes[cls_indices]
-            cls_box_classes = box_classes[cls_indices]
-            cls_box_scores = box_scores[cls_indices]
+            while len(class_boxes) > 0:
+                max_score_index = np.argmax(class_scores)
 
-            ranked = np.argsort(cls_box_scores)[::-1]
-            cls_boxes = cls_boxes[ranked]
-            cls_box_classes = cls_box_classes[ranked]
-            cls_box_scores = cls_box_scores[ranked]
+                box_predictions.append(class_boxes[max_score_index])
+                predicted_box_classes.append(cls)
+                predicted_box_scores.append(class_scores[max_score_index])
 
-            while len(cls_box_scores) > 0:
-                box_predictions.append(cls_boxes[0])
-                predicted_box_classes.append(cls_box_classes[0])
-                predicted_box_scores.append(cls_box_scores[0])
+                ious = np.array([
+                    self.iou(class_boxes[max_score_index], box)
+                    for box in class_boxes
+                ])
 
-                if len(cls_box_scores) == 1:
-                    break
+                remove = np.where(ious > self.nms_t)[0]
 
-                x1 = np.maximum(cls_boxes[0, 0], cls_boxes[1:, 0])
-                y1 = np.maximum(cls_boxes[0, 1], cls_boxes[1:, 1])
-                x2 = np.minimum(cls_boxes[0, 2], cls_boxes[1:, 2])
-                y2 = np.minimum(cls_boxes[0, 3], cls_boxes[1:, 3])
-
-                inter_w = np.maximum(0, x2 - x1)
-                inter_h = np.maximum(0, y2 - y1)
-                intersection = inter_w * inter_h
-
-                box_area = (
-                    (cls_boxes[0, 2] - cls_boxes[0, 0]) *
-                    (cls_boxes[0, 3] - cls_boxes[0, 1])
-                )
-                other_areas = (
-                    (cls_boxes[1:, 2] - cls_boxes[1:, 0]) *
-                    (cls_boxes[1:, 3] - cls_boxes[1:, 1])
-                )
-
-                union = box_area + other_areas - intersection
-                iou = intersection / union
-
-                keep = np.where(iou < self.nms_t)[0]
-
-                cls_boxes = cls_boxes[keep + 1]
-                cls_box_classes = cls_box_classes[keep + 1]
-                cls_box_scores = cls_box_scores[keep + 1]
+                class_boxes = np.delete(class_boxes, remove, axis=0)
+                class_scores = np.delete(class_scores, remove)
 
         box_predictions = np.array(box_predictions)
         predicted_box_classes = np.array(predicted_box_classes)
@@ -164,10 +158,11 @@ class Yolo:
         images = []
         image_paths = []
 
-        for filename in sorted(os.listdir(folder_path)):
-            path = os.path.join(folder_path, filename)
-            if os.path.isfile(path):
+        for filename in os.listdir(folder_path):
+            if filename.endswith((".jpg", ".jpeg", ".png", ".bmp")):
+                path = os.path.join(folder_path, filename)
                 image = cv2.imread(path)
+
                 if image is not None:
                     images.append(image)
                     image_paths.append(path)
@@ -176,19 +171,21 @@ class Yolo:
 
     def preprocess_images(self, images):
         """Resize and rescale images for YOLO input"""
-        input_h = self.model.input.shape[1]
-        input_w = self.model.input.shape[2]
-
         pimages = []
         image_shapes = []
 
+        input_h = self.model.input.shape[1]
+        input_w = self.model.input.shape[2]
+
         for image in images:
             image_shapes.append(image.shape[:2])
+
             resized = cv2.resize(
                 image,
-                (input_w, input_h),
+                (input_h, input_w),
                 interpolation=cv2.INTER_CUBIC
             )
+
             pimages.append(resized / 255.0)
 
         pimages = np.array(pimages)
