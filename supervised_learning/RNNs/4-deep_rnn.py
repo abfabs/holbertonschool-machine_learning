ccc6c9a5251f2244f8303b0130a21b562e33c1a7
abfabs <albaeftimi@gmail.com>
2026-04-20 12:32:37 +0200

    py

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -8,7 +8,8 @@ def deep_rnn(rnn_cells, X, h_0):
     Perform forward propagation for a deep RNN.
 
     Args:
-        rnn_cells: list of RNNCell instances of length l for forward propagation
+        rnn_cells: list of RNNCell instances of length l for forward
+            propagation
         X: numpy.ndarray of shape (t, m, i) containing the input data
         h_0: numpy.ndarray of shape (l, m, h) containing initial hidden states
 
@@ -17,17 +18,18 @@ def deep_rnn(rnn_cells, X, h_0):
         Y: numpy.ndarray of shape (t, m, o) containing all outputs
     """
     t, m, _ = X.shape
-    l = len(rnn_cells)
+    layers = len(rnn_cells)
     h = h_0.shape[2]
 
-    H = np.zeros((t + 1, l, m, h))
+    H = np.zeros((t + 1, layers, m, h))
     H[0] = h_0
 
     outputs = []
     for step in range(t):
         layer_input = X[step]
-        for layer in range(l):
-            h_next, y = rnn_cells[layer].forward(H[step, layer], layer_input)
+        for layer in range(layers):
+            h_next, y = rnn_cells[layer].forward(
+                H[step, layer], layer_input)
             H[step + 1, layer] = h_next
             layer_input = h_next
         outputs.append(y)
