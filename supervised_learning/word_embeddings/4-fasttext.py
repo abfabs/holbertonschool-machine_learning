bbcc4eefbf81d70f10f78415271caee2a2cb6f49
abfabs <albaeftimi@gmail.com>
2026-04-27 22:36:03 +0200

    m

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -10,21 +10,12 @@ def fasttext_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5, cbow=True,
                    epochs=5, seed=0, workers=1):
     """
-    Creates, builds, and trains a gensim FastText model.
-
-    sentences: list of sentences to be trained on
-    vector_size: dimensionality of the embedding layer
-    min_count: minimum number of occurrences of a word for use in training
-    window: maximum distance between the current and predicted word
-    negative: size of negative sampling
-    cbow: True for CBOW; False for Skip-gram
-    epochs: number of iterations to train over
-    seed: seed for the random number generator
-    workers: number of worker threads to train the model
-
-    Returns: the trained model
+    info
     """
+    # Set the training algorithm
     sg = 0 if cbow else 1
+
+    # Create the FastText model
     model = gensim.models.FastText(
         sentences=sentences,
         vector_size=vector_size,
@@ -35,5 +26,13 @@ def fasttext_model(sentences, vector_size=100, min_count=5,
         epochs=epochs,
         seed=seed,
         workers=workers
-    )
+        )
+
+    # prepare vocabulary
+    model.build_vocab(sentences)
+
+    # Train
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
+
     return model
